2b510ed0f6179cf8bd9b66a264aea635599f1390
fridajan <frida.k.jansson@gmail.com>
2013-12-16 16:04:57 +0100

    changed something in room

--- lab3/room.h
+++ lab3/room.h
@@ -5,27 +5,52 @@
 #include <stdexcept>
 #include <string>
 
+//TODO: makefile
+
 namespace haunted_house 
 {
   class Room {
   private:
+    std::string m_description;
   	std:vector<int> m_directions;
-  	std:map<std::string, Item> m_items;
+    std::std::vector<Character*> m_characters;
+  	std:map<std::string, Item*> m_items;
+    std:map<int, Room> m_neighbours;
 
   public:
+    enum Direction {
+      north, east, south, west, northeast, northwest, southeast, southwest;
+    };
+
     virtual ~Rooms() { };
 
-    virtual std::vector<int> directions() const =0;
+    virtual std::vector<int> directions() const {
+      return m_directions;
+    };
 
-    virtual Room& neighbour(int direction) const =0;
+    virtual Room& getNeighbour(int direction) const {
+      if(m_neighbours.find(direction) == m_neighbours.end()) {
+        return *m_neighbours[direction];
+      } else {
+        return NULL;
+      }
+    };
 
-    virtual std::string description() =0;
+    virtual std::string description() {
+      return m_description;
+    };
 
     virtual bool enter(Character& c) =0;
 
     virtual bool leave(Character& c) =0;
 
-    virtual bool pick_up(Item& it) =0;
+    virtual bool pick_up(std::string it) {
+      if(m_items.find(direction) == m_neighbours.end()) {
+        return true;
+      } else {
+        return false;
+      }
+    };
 
     virtual bool drop(Item& it) =0;
 
